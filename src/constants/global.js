bd90690d0074c96a23c2c0a7ada05055e8086fb7
Mario Krstevski <mariokrstevski@hotmail.com>
2023-11-29 06:47:54 +0100

    change to live mode

--- src/constants/global.js
+++ src/constants/global.js
@@ -5,8 +5,8 @@ const localFrontendURL = "http://localhost:3000/";
 const publishedFrontendUrl =
   "https://main--neon-faun-85f35c.netlify.app/";
 
-// export const frontendBaseUrl = publishedFrontendUrl;
-// export const backendBaseUrl = publishedUrl;
+export const frontendBaseUrl = publishedFrontendUrl;
+export const backendBaseUrl = publishedUrl;
 
-export const frontendBaseUrl = localFrontendURL;
-export const backendBaseUrl = localUrl;
+// export const frontendBaseUrl = localFrontendURL;
+// export const backendBaseUrl = localUrl;
