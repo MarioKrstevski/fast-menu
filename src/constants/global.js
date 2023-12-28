f2b14ad04ba968df1a50f8e036a0b3879d182335
Mario Krstevski <mariokrstevski@hotmail.com>
2023-12-28 09:54:22 +0100

    globals change

--- src/constants/global.js
+++ src/constants/global.js
@@ -2,8 +2,7 @@ const localUrl = "http://localhost:8000/";
 const publishedUrl = "https://api-omjz.onrender.com/";
 
 const localFrontendURL = "http://localhost:3000/";
-const publishedFrontendUrl =
-  "https://main--neon-faun-85f35c.netlify.app/";
+const publishedFrontendUrl = "https://neon-faun-85f35c.netlify.app/";
 
 export const frontendBaseUrl = publishedFrontendUrl;
 export const backendBaseUrl = publishedUrl;
